64a8e4bf1142604d3d983e3c03edf574bf16c7eb
Josef Fort <fort.josef@gmail.com>
2020-07-16 16:29:46 +0300

    Global process lesson

--- globalProcess.js
+++ globalProcess.js
@@ -1,2 +1,8 @@
 console.log(process.pid);
-console.log(process.version);
+console.log(process.versions.node);
+console.log(process.argv);
+
+//you can so the equivilant of a php list with this
+const [nodeloc, scriptloc, firstarg, lastarg] = process.argv;
+
+console.log(`fisrstarg ${firstarg} secondarg ${lastarg}`);
