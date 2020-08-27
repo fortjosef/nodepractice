35ef035d54690008439577c6aaeac8aa5f80ddbc
Josef Fort <fort.josef@gmail.com>
2020-08-27 14:53:40 +0300

    Write streams

--- streams.js
+++ streams.js
@@ -12,3 +12,11 @@ readStream.once('data', (data) => {
 readStream.on('end', () => {
     console.log('finished');
 });
+
+//appears to overwrite if file exists
+const writeStream = fs.createWriteStream('writestream.txt', 'utf-8');
+
+writeStream.write('test write 2');
+
+//doesnt write untill enter is pressed
+process.stdin.pipe(writeStream);
